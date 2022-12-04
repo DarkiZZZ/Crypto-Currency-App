f54a3054b0f9e27deb03c51ceabdb3ab82852c6a
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-04 21:57:13 +0300

    updated RestModule.kt with provide functions OkHttpClient, Retrofit, ApiService

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/di/RestModule.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/di/RestModule.kt
@@ -1,7 +1,53 @@
 package ru.msokolov.cryptocurrencyapp.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.msokolov.cryptocurrencyapp.rest.CoinGeckoApi
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class RestModule {
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().setLenient().create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().
+                addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)).
+                connectTimeout(60, TimeUnit.SECONDS).
+                build()
+    }
+
+    @Provides
+    @Singleton
+    @Named("COINGECKO_API")
+    fun provideGeckoRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/api/v3/")// OR https://pro-api.coingecko.com/api/v3/
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGeckoApiService(@Named("COINGECKO_API") retrofit: Retrofit): CoinGeckoApi {
+        return retrofit.create(CoinGeckoApi::class.java)
+    }
+
 }
