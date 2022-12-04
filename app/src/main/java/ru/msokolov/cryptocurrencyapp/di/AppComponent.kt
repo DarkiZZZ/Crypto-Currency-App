26a4556d5f17ad21339e9d0434f5a5ba95c7b75e
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-04 21:57:54 +0300

    updated with inject function and annotations

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/di/AppComponent.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/di/AppComponent.kt
@@ -1,4 +1,14 @@
 package ru.msokolov.cryptocurrencyapp.di
 
+import dagger.Component
+import ru.msokolov.cryptocurrencyapp.MainActivity
+import javax.inject.Singleton
+
+
+@Component(modules = arrayOf(AppModule::class, RestModule::class,
+    MvpModule::class, ChartModule::class))
+@Singleton
 interface AppComponent {
+
+    fun inject(mainActivity: MainActivity)
 }
