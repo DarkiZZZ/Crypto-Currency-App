0cf6f21481d8aa31876836a921c544391e2e286c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-05 22:09:24 +0300

    added providers to mvp module & component

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/di/AppComponent.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/di/AppComponent.kt
@@ -2,6 +2,9 @@ package ru.msokolov.cryptocurrencyapp.di
 
 import dagger.Component
 import ru.msokolov.cryptocurrencyapp.MainActivity
+import ru.msokolov.cryptocurrencyapp.mvp.contract.CurrenciesContract
+import ru.msokolov.cryptocurrencyapp.mvp.presenter.CurrenciesPresenter
+import ru.msokolov.cryptocurrencyapp.mvp.presenter.LatestChartPresenter
 import javax.inject.Singleton
 
 
@@ -11,4 +14,6 @@ import javax.inject.Singleton
 interface AppComponent {
 
     fun inject(mainActivity: MainActivity)
+    fun inject(presenter: CurrenciesPresenter)
+    fun inject(presenter: LatestChartPresenter)
 }
--- app/src/main/java/ru/msokolov/cryptocurrencyapp/di/MvpModule.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/di/MvpModule.kt
@@ -1,7 +1,19 @@
 package ru.msokolov.cryptocurrencyapp.di
 
 import dagger.Module
+import dagger.Provides
+import ru.msokolov.cryptocurrencyapp.mvp.presenter.CurrenciesPresenter
+import ru.msokolov.cryptocurrencyapp.mvp.presenter.LatestChartPresenter
+import javax.inject.Singleton
 
 @Module
 class MvpModule {
+
+    @Provides
+    @Singleton
+    fun provideCurrenciesPresenter(): CurrenciesPresenter = CurrenciesPresenter()
+
+    @Provides
+    @Singleton
+    fun provideLatestChartPresenter(): LatestChartPresenter = LatestChartPresenter()
 }
