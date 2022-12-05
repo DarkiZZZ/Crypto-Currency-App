a67436be09d078f931b9e0ee068fae35ba535f3f
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-05 21:41:14 +0300

    MVP base Contract added

---
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/mvp/contract/BaseContract.kt
@@ -0,0 +1,32 @@
+package ru.msokolov.cryptocurrencyapp.mvp.contract
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.Flow.Subscription
+
+class BaseContract {
+
+    interface View
+
+    abstract class Presenter<V: View> {
+        private val subscriptions = CompositeDisposable()
+        protected lateinit var view: V
+
+        fun subscribe(subscription: Disposable) {
+            subscriptions.add(subscription)
+        }
+
+        fun unsubscribe(){
+            subscriptions.clear()
+        }
+
+        fun attach(view: V){
+            this.view = view
+        }
+
+        fun detach(){
+            unsubscribe()
+        }
+    }
+
+}
