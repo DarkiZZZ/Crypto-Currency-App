c6aca17d5fcfa9ce3d0ba3a53bd83d17871c5b4c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-05 22:53:09 +0300

    filled Currencies and LatestChart presenters

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/mvp/presenter/CurrenciesPresenter.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/mvp/presenter/CurrenciesPresenter.kt
@@ -1,4 +1,68 @@
 package ru.msokolov.cryptocurrencyapp.mvp.presenter
 
-class CurrenciesPresenter {
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import ru.msokolov.cryptocurrencyapp.adapter.entities.Currency
+import ru.msokolov.cryptocurrencyapp.di.App
+import ru.msokolov.cryptocurrencyapp.formatters.formatThousands
+import ru.msokolov.cryptocurrencyapp.mvp.contract.CurrenciesContract
+import ru.msokolov.cryptocurrencyapp.rest.CoinGeckoApi
+import javax.inject.Inject
+
+class CurrenciesPresenter: CurrenciesContract.Presenter() {
+
+    @Inject
+    lateinit var geckoApi: CoinGeckoApi
+
+    init {
+        App.appComponent.inject(this)
+    }
+
+    override fun makeList() {
+        view.showProgress()
+        subscribe(geckoApi.getCoinMarket()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .flatMap { Observable.fromIterable(it) }
+            .doOnNext{
+                view.addCurrency(
+                    Currency(
+                        it.id,
+                        it.symbol,
+                        it.name,
+                        it.image,
+                        it.current_price,
+                        it.market_cap.formatThousands(),
+                        it.market_cap_rank,
+                        it.total_volume,
+                        it.price_change_percentage_24h,
+                        it.market_cap_change_percentage_24h,
+                        it.circulating_supply,
+                        it.total_supply,
+                        it.ath,
+                        it.ath_change_percentage
+                    )
+                )
+            }
+            .doOnComplete{
+                view.hideProgress()
+            }
+            .subscribe({
+                view.hideProgress()
+                view.notifyAdapter()
+            }, {
+                view.showErrorMessage(it.message)
+                view.hideProgress()
+                it.printStackTrace()
+            })
+        )
+    }
+
+    override fun refreshList() {
+        view.refresh()
+        makeList()
+    }
+
+
 }
