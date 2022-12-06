f53cd76535549387bcb81c55c1984cf9dea378db
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-06 13:12:42 +0300

    new activity for chart created

---
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/activities/ChartActivity.kt
@@ -0,0 +1,12 @@
+package ru.msokolov.cryptocurrencyapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.msokolov.cryptocurrencyapp.R
+
+class ChartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart)
+    }
+}
