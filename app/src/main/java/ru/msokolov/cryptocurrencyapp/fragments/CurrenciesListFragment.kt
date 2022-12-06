fb7454bb43fc18922578668bf0e75173eb269d43
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-06 11:29:31 +0300

    filled fragments with view methods

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/fragments/CurrenciesListFragment.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/fragments/CurrenciesListFragment.kt
@@ -4,16 +4,73 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.msokolov.cryptocurrencyapp.R
+import ru.msokolov.cryptocurrencyapp.adapter.BaseAdapter
+import ru.msokolov.cryptocurrencyapp.adapter.CurrenciesAdapter
+import ru.msokolov.cryptocurrencyapp.adapter.entities.Currency
+import ru.msokolov.cryptocurrencyapp.di.App
+import ru.msokolov.cryptocurrencyapp.mvp.contract.CurrenciesContract
+import ru.msokolov.cryptocurrencyapp.mvp.presenter.CurrenciesPresenter
+import javax.inject.Inject
 
-class CurrenciesListFragment: Fragment(){
+class CurrenciesListFragment : BaseListFragment(), CurrenciesContract.View {
+
+    @Inject
+    lateinit var presenter: CurrenciesPresenter
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_currencies_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        App.appComponent.inject(this)
+        presenter.attach(this)
+        presenter.makeList()
+    }
+
+    override fun createAdapterInstance(): BaseAdapter<*> {
+        return CurrenciesAdapter()
+    }
+
+    override fun addCurrency(currency: Currency) {
+        viewAdapter.add(currency)
+    }
+
+    override fun notifyAdapter() {
+        viewAdapter.notifyDataSetChanged()
+    }
+
+    override fun showProgress() {
+        requireActivity().progress.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        requireActivity().progress.visibility = View.INVISIBLE
+    }
+
+    override fun showErrorMessage(error: String?) {
+        Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun refresh() {
+        viewAdapter.items.clear()
+        viewAdapter.notifyDataSetChanged()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.attach(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.detach()
     }
 }
