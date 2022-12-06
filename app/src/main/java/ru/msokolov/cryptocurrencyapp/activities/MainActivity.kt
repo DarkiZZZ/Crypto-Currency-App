fd7cdcbf500a1434d110f7c03cb7b0615d17bd11
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-12-06 11:31:36 +0300

    fixed problem with infinite progressbar -> initial state for fragment added, added internet permission

--- app/src/main/java/ru/msokolov/cryptocurrencyapp/activities/MainActivity.kt
+++ app/src/main/java/ru/msokolov/cryptocurrencyapp/activities/MainActivity.kt
@@ -1,16 +1,24 @@
-package ru.msokolov.cryptocurrencyapp
+package ru.msokolov.cryptocurrencyapp.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import ru.msokolov.cryptocurrencyapp.activities.AboutActivity
+import ru.msokolov.cryptocurrencyapp.R
+import ru.msokolov.cryptocurrencyapp.fragments.CurrenciesListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, CurrenciesListFragment(), null)
+                .commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
